43ec4b1a9a9be8c10dc20fc2f554b47d12829f52
pira019 <pires00243@gmail.com>
2022-01-26 10:36:11 +0100

    slide nav

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="sidebar">
     <div class="sidebar-wrapper">
         <div class="logo">
-            <a href="#" class="simple-text logo-mini">{{ __('BD') }}</a>
-            <a href="#" class="simple-text logo-normal">{{ __('Black Dashboard') }}</a>
+            <a href="#" class="simple-text logo-mini">{{ __('I') }}</a>
+            <a href="#" class="simple-text logo-normal">{{ __('ISCHOOL') }}</a>
         </div>
         <ul class="nav">
             <li @if ($pageSlug == 'dashboard') class="active " @endif>
@@ -11,31 +11,31 @@
                     <p>{{ __('Dashboard') }}</p>
                 </a>
             </li>
-            <li>
-                <a data-toggle="collapse" href="#laravel-examples" aria-expanded="true">
+           <!--   <li>
+              <a data-toggle="collapse" href="#laravel-examples" aria-expanded="true">
                     <i class="fab fa-laravel" ></i>
                     <span class="nav-link-text" >{{ __('Laravel Examples') }}</span>
                     <b class="caret mt-1"></b>
                 </a>
 
                 <div class="collapse show" id="laravel-examples">
-                    <ul class="nav pl-4">
+                    <ul class="nav pl-4">!-->
                         <li @if ($pageSlug == 'profile') class="active " @endif>
                             <a href="{{ route('profile.edit')  }}">
                                 <i class="tim-icons icon-single-02"></i>
-                                <p>{{ __('User Profile') }}</p>
+                                <p>{{ __('Dosier étudiants') }}</p>
                             </a>
                         </li>
                         <li @if ($pageSlug == 'users') class="active " @endif>
                             <a href="{{ route('user.index')  }}">
                                 <i class="tim-icons icon-bullet-list-67"></i>
-                                <p>{{ __('User Management') }}</p>
+                                <p>{{ __('Assiduité') }}</p>
                             </a>
                         </li>
-                    </ul>
-                </div>
-            </li>
-            <li @if ($pageSlug == 'icons') class="active " @endif>
+                 <!--   </ul>
+                 </div>
+            </li>!-->
+         <!--   <li @if ($pageSlug == 'icons') class="active " @endif>
                 <a href="{{ route('pages.icons') }}">
                     <i class="tim-icons icon-atom"></i>
                     <p>{{ __('Icons') }}</p>
@@ -76,7 +76,7 @@
                     <i class="tim-icons icon-spaceship"></i>
                     <p>{{ __('Upgrade to PRO') }}</p>
                 </a>
-            </li>
+            </li>!-->
         </ul>
     </div>
 </div>
