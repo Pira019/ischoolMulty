e65b68d6822da2d4889247c2d118d3629920a3ec
pira019 <pires00243@gmail.com>
2022-02-11 18:34:59 +0100

    permittion files

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,17 @@
 
 namespace App\Models;
 
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Spatie\Multitenancy\Models\Concerns\UsesTenantConnection;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, UsesTenantConnection ;
+    use HasApiTokens, HasFactory, Notifiable, UsesTenantConnection, HasRoles;
 
     /**
      * The attributes that are mass assignable.
