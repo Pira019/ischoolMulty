204f53ca48d78c120156e1926282e24d912d2939
pira019 <pires00243@gmail.com>
2022-03-01 13:39:19 +0100

    lang fr

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -15,6 +15,4 @@ return [
     'failed'   => 'Ces identifiants ne correspondent pas à nos enregistrements.',
     'password' => 'Le mot de passe fourni est incorrect.',
     'throttle' => 'Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.',
-    'Login' => 'connexion',
-    'forgot your password?' => 'Mot de passe oublié?',
 ];
